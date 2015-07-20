62d65fbbc4980e72be70422c774c5bee855ba44b
Nicola Cimmino <nicola@parkopedia.com>
2015-07-20 21:40:04 +0100

    Updae docs

--- apidocs/api_project.js
+++ apidocs/api_project.js
@@ -6,7 +6,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-07-20T20:31:54.574Z",
+    "time": "2015-07-20T20:39:47.854Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
